17576691de71a5384f6d19c7b44ae0f7d7afc630
Dan <DEVD2@protonmail.com>
2022-11-30 21:10:08 +1000

    encapsulate run code with "main" method as per best practice

--- dir_checker.py
+++ dir_checker.py
@@ -92,28 +92,29 @@ def send_email(cfg, emailToSend):
             status = True
     return status
 
+# run script from main as per best practice    
+if __name__ == "__main__":
+    cfg = config.load_config()
 
-cfg = config.load_config()
+    # get the contents of the directory
+    contents = get_directory_contents(cfg.directoryToMonitor)
+    if len(contents) == 0:  # do nothing
+        print("There are no files in the specified folder. Exiting. Biyeee!")
+        sys.exit()
 
-# get the contents of the directory
-contents = get_directory_contents(cfg.directoryToMonitor)
-if len(contents) == 0:  # do nothing
-    print("There are no files in the specified folder. Exiting. Biyeee!")
-    sys.exit()
-
-now = datetime.datetime.now()
-# there are files so time to notify someone about it
-dirName = os.path.basename(os.path.normpath(cfg.directoryToMonitor))
-emailText = (
-    "There are {} files in the directory '{}' that are ready to be"
-    " reviewed as of {}.".format(
-        len(contents),
-        dirName,
-        now.strftime("%b %d %Y %H:%M:%S"),
+    now = datetime.datetime.now()
+    # there are files so time to notify someone about it
+    dirName = os.path.basename(os.path.normpath(cfg.directoryToMonitor))
+    emailText = (
+        "There are {} files in the directory '{}' that are ready to be"
+        " reviewed as of {}.".format(
+            len(contents),
+            dirName,
+            now.strftime("%b %d %Y %H:%M:%S"),
+        )
     )
-)
-emailSent = send_email(cfg, emailText)
-if not emailSent:
-    print("Unable to send email. See log for errors.")
-else:
-    print("Email sent successfully. Goodbye!")
+    emailSent = send_email(cfg, emailText)
+    if not emailSent:
+        print("Unable to send email. See log for errors.")
+    else:
+        print("Email sent successfully. Goodbye!")    
